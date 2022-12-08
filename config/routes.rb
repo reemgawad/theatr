a6d4ce4be89416307ec29f587a2cb7197fee0f54
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2022-12-08 15:37:08 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
   devise_for :users
   root to: "pages#home"
 
   get "/play", to: 'pages#play', as: :play
+
+  resources :profiles, only: [ :show ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
