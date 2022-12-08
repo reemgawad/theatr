136d4158a53ee2a5209a62aa9b0f287bb14c5fc8
Reem Gawad <reemabdelg@gmail.com>
2022-12-08 18:16:49 -0500

    implemented creation of user response for each activity question

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,6 +2,7 @@ class ActivitiesController < ApplicationController
   def show
     @activity = Activity.find(params[:id])
     @user_response = UserResponse.new()
+    authorize @activity
   end
 
   def index
--- app/controllers/user_responses_controller.rb
+++ app/controllers/user_responses_controller.rb
@@ -3,22 +3,23 @@ class UserResponsesController < ApplicationController
 
   def create
     @user_response = UserResponse.new(user_response_params)
-    @user_response.activity = @activity
     @user_response.user = current_user
-    if @user_response.save
+    if @user_response.save!
       redirect_to activity_path(@activity)
     else
       render 'activities/show', status: :unprocessable_entity
     end
+    authorize @user_response
   end
 
   private
 
   def user_response_params
-    params.require(:user_response).permit(:text)
+    params.require(:user_response).permit(:text, :activity_question_id)
   end
 
   def set_activity
-    @activity_question = ActivityQuestion.find(params[:id])
+    activity_question = ActivityQuestion.find(params[:activity_question_id])
+    @activity = Activity.find(activity_question.activity.id)
   end
 end
