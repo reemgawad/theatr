9dfa24a344edd01c0b7fa8bcba6907aef6eaf89c
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2022-12-09 13:30:46 -0500

    we can now create teacher_comment

--- app/controllers/teacher_comments_controller.rb
+++ app/controllers/teacher_comments_controller.rb
@@ -1,2 +1,26 @@
 class TeacherCommentsController < ApplicationController
+  before_action :set_user, only: [ :create ]
+
+  def create
+    @teacher_comment = TeacherComment.new(teacher_comment_params)
+    @teacher_comment.teacher = current_user
+    authorize @teacher_comment
+    if @teacher_comment.save
+      redirect_to results_path(@user, @activity)
+    else
+      render 'activities/results', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def teacher_comment_params
+    params.require(:teacher_comment).permit(:text, :user_response_id)
+  end
+
+  def set_user
+    @user_response = UserResponse.find(params[:user_response_id])
+    @user = @user_response.user
+    @activity = @user_response.activity_question.activity
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,19 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   get "/play", to: 'pages#play', as: :play
+  # post "/user_responses/:id/teacher_comments", to: "teacher_comments#create", as:
 
   get "/profiles/:user_id/activities/:id/results", to: "activities#results", as: :results
   resources :profiles, only: [ :show ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
-  resources :activities, only: [:show, :index]
+
+  resources :activities, only: [ :show, :index ]
+
+  resources :user_responses do
+    resources :teacher_comments, only: [ :create ]
+  end
 
   resources :activity_questions do
-    resources :user_responses, only: [:create]
+    resources :user_responses, only: [ :create ]
   end
 end
