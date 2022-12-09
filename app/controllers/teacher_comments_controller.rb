1d1229bdf900f4a8abe5fa9843c5681345979f68
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2022-12-09 13:39:48 -0500

    added link to the profile page to see results if it's your ptofile or you are a teacher

--- app/controllers/teacher_comments_controller.rb
+++ app/controllers/teacher_comments_controller.rb
@@ -4,7 +4,9 @@ class TeacherCommentsController < ApplicationController
   def create
     @teacher_comment = TeacherComment.new(teacher_comment_params)
     @teacher_comment.teacher = current_user
+
     authorize @teacher_comment
+
     if @teacher_comment.save
       redirect_to results_path(@user, @activity)
     else
@@ -19,6 +21,8 @@ class TeacherCommentsController < ApplicationController
   end
 
   def set_user
+    # find user and activity to redirect to same page, we will need java to stop refresh
+    # here as well
     @user_response = UserResponse.find(params[:user_response_id])
     @user = @user_response.user
     @activity = @user_response.activity_question.activity
