641bf51e5e370a1d1ad3bb9d46250ce40ed53638
Reem Gawad <reemabdelg@gmail.com>
2022-12-13 12:21:13 -0500

    added available column to activities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_13_165523) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_13_172001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_13_165523) do
     t.datetime "updated_at", null: false
     t.string "activity_type"
     t.string "level"
+    t.boolean "available"
   end
 
   create_table "activity_questions", force: :cascade do |t|
