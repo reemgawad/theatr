a6f43f17888bb3ab54f3d0556cc40b294cde98c8
Reem Gawad <reemabdelg@gmail.com>
2022-12-20 03:54:16 +0300

    added logic to set color of badge according to its status

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -5,5 +5,16 @@ class ClassroomsController < ApplicationController
     @teacher = User.find_by(classroom: @classroom, teacher: true)
     @students = User.where(classroom: @classroom, teacher: false)
     authorize @user, policy_class: ClassroomPolicy
+    set_color
+  end
+
+  def set_color
+    @color = {
+      unavailable: 'red',
+      available: 'blue',
+      started: 'orange',
+      marked: 'purple',
+      completed: 'green'
+    }
   end
 end
