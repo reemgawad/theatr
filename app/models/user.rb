f26d5e56c62b79edb60af8c34d8cf26541fe9f29
Reem Gawad <reemabdelg@gmail.com>
2022-12-20 23:06:44 +0300

    added logic for classroom generation upon teacher signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,27 @@ class User < ApplicationRecord
   has_many :teacher_comments, foreign_key: :student_id
   belongs_to :classroom
 
-  before_create :validate_access_code
+  validates :access_code, presence: true
+
+  before_save :validate_access_code
+  after_create :generate_classroom
 
   def validate_access_code
+    if self.access_code == "placeholderTC"
+      self.teacher = true
+    else
+      # get all Classrooms access_codes and check if access code entered is one of them
+      # if yes, self.classroom = that classroom
+      # if not, access code is incorrect
+    end
+  end
 
+  def generate_classroom
+    if self.teacher == true
+      classroom = Classroom.create!(date: DateTime.new(2022, 12, 14, 5, 30))
+      classroom.access_code = "class##{classroom.id}"
+      classroom.save!
+      self.classroom = classroom
+    end
   end
 end
