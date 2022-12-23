b1e64cb8662aab479c8d3a77720e86c7c17eb743
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2022-12-23 10:12:03 -0500

    need to change migrations

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -2,9 +2,6 @@ class Badge < ApplicationRecord
   validates :user, uniqueness: { scope: :activity }
   belongs_to :user
   belongs_to :activity
-  has_one :classroom, through: :user
-
-  has_and_belongs_to_many :classroom, through: :user
 
   enum status: {
     unavailable: 0,
