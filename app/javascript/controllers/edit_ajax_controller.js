f899a4518f02af02e9dd255d685e07db1e83f4a7
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2023-01-04 15:14:25 -0500

    just need to make the edit appear

--- app/javascript/controllers/edit_ajax_controller.js
+++ app/javascript/controllers/edit_ajax_controller.js
@@ -2,14 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="edit-ajax"
 export default class extends Controller {
-  static targets = ["responseInfo", "responseEdit", "form", "content"]
+  static targets = ["responseInfo", "responseEdit", "form", "question"]
   static values = {
     activityId: Number,
     userId: Number
   }
 
   connect() {
-    console.log(this.formTargetsAll)
+
   }
 
   revealForm(event) {
@@ -21,17 +21,17 @@ export default class extends Controller {
 
   preventRefresh(event) {
     event.preventDefault();
-    let url = "/user_responses"
+
     let answer = (this.formTarget.querySelector('input[name="user_response[text]"]:checked').value);
-
-    console.log(answer)
-
-    console.log(this.userIdValue)
 
     fetch(this.formTarget.action, {
       method: 'POST',
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify({"text": answer, "activity_question_id": this.activityIdValue, "user_id": this.userIdValue, "correct": false,})
     })
+
+    this.formTarget.classList.add("d-none")
+
+    this.questionTarget.innerHTML += `<p>${answer}</p>`
   }
 }
