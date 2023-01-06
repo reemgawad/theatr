45653e12ccddda17350cf7693ab045e73b235c06
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2023-01-05 21:41:14 -0500

    removed console.log

--- app/javascript/controllers/classroom_activity_controller.js
+++ app/javascript/controllers/classroom_activity_controller.js
@@ -12,20 +12,17 @@ export default class extends Controller {
 
   changeButton(event) {
     event.preventDefault();
-    console.log(this.formTarget.querySelector("input"))
 
     if (this.formTarget.querySelector("input").classList.contains('btn-primary')) {
       fetch(this.formTarget.action)
       this.formTarget.action = `/activities/${this.activityIdValue}/remove_activity`
       this.formTarget.querySelector("input").classList = "btn btn-danger"
       this.formTarget.querySelector("input").value = "Remove Activity"
-      console.log(this.formTarget)
     } else {
       fetch(this.formTarget.action)
       this.formTarget.action = `/activities/${this.activityIdValue}/add_activity`
       this.formTarget.querySelector("input").classList = "btn btn-primary"
       this.formTarget.querySelector("input").value = "Add Activity"
-      console.log(this.formTarget)
     }
   }
 }
