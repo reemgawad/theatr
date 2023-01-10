edeb977f93d29a86506611a8d3f935cd6bb2d355
Reem Gawad <reemabdelg@gmail.com>
2023-01-10 12:42:00 -0500

    added has_many_attached: files to activity & activity_question

--- app/models/activity_question.rb
+++ app/models/activity_question.rb
@@ -2,5 +2,5 @@ class ActivityQuestion < ApplicationRecord
   belongs_to :activity
   has_many :user_responses
   validates_associated :activity
-  has_many_attached :photos
+  has_many_attached :files
 end
