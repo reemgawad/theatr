329816f1d1eb0c1b93512f0d31f48b964f75de70
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2023-02-02 18:51:58 -0500

    works for one click but have to refresh

--- app/assets/javascript/submit.js
+++ app/assets/javascript/submit.js
@@ -5,8 +5,16 @@ var submitAll = document.querySelector('#submit-all')
 // console.log(submitAll);
 
 submitAll.addEventListener('click', (event) => {
+  // event.preventDefault()
+
   allSubmitButtons.forEach((e) => {
     // console.log(e);
     e.click();
   })
+
+  // fetch(submitAll.parentElement.action)
+
+  // window.location.reload()
+
+
 })
